d822578733b35a37e50cd4c23044c65fd3e807bd
deltager <github@software.unf.dk>
2017-07-06 12:25:29 +0200

    Initial commit

---
+++ app/src/main/java/software/unf/dk/simonsaysworkingproject/MainActivity.java
@@ -0,0 +1,32 @@
+package software.unf.dk.simonsaysworkingproject;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button greenButton, orangeButton, redButton, blueButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        greenButton = (Button) findViewById(R.id.greenButton);
+        orangeButton = (Button) findViewById(R.id.orangeButton);
+        redButton = (Button) findViewById(R.id.redButton);
+        blueButton = (Button) findViewById(R.id.blueButton);
+
+    }
+
+    public void blue(View view) {
+        blueButton.setBackgroundColor(Color.CYAN);
+    }
+
+
+
+
+}
