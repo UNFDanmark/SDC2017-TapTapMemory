f9b0c89c084a9e1af473d82b59250f0cd0934b92
deltager <github@software.unf.dk>
2017-07-06 13:59:04 +0200

    Random generator added (hopefully)

--- app/src/main/java/software/unf/dk/simonsaysworkingproject/MainActivity.java
+++ app/src/main/java/software/unf/dk/simonsaysworkingproject/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button greenButton, orangeButton, redButton, blueButton, start;
 
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
