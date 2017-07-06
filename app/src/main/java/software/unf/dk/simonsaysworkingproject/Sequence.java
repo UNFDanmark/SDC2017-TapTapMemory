d480b08582435e031e5fba3e43e4b39b146edaec
deltager <github@software.unf.dk>
2017-07-06 15:29:51 +0200

    Added sequence and changed generater to timebased

--- app/src/main/java/software/unf/dk/simonsaysworkingproject/Sequence.java
+++ app/src/main/java/software/unf/dk/simonsaysworkingproject/Sequence.java
@@ -1,6 +1,7 @@
 package software.unf.dk.simonsaysworkingproject;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by deltager on 06-07-17.
@@ -8,6 +9,9 @@ import java.util.ArrayList;
 
 public class Sequence {
     ArrayList<Integer> sequence = new ArrayList<>();
+    Random generator = new Random();
+    int rnr = (generator.nextInt(4));
+
 
 
 }
