ee4984d316594a8ef8b90e8faea7a744fda85065
deltager <github@software.unf.dk>
2017-07-08 23:07:44 +0200

    Made store and transition ...

--- app/src/main/java/software/unf/dk/simonsaysworkingproject/StoreActivity.java
+++ app/src/main/java/software/unf/dk/simonsaysworkingproject/StoreActivity.java
@@ -3,6 +3,7 @@ package software.unf.dk.simonsaysworkingproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
@@ -29,6 +30,9 @@ public class StoreActivity extends AppCompatActivity {
     public void newLevel(View view) {
         if (count >= 20) {
             startActivity(new Intent(StoreActivity.this, SecondActivity.class));
+            /*count -= 20;
+            System.out.println(count);
+            Log.i("Find mig", "Find mig"); */
         } else {
             Toast.makeText(getApplicationContext(), "Not enough points", Toast.LENGTH_SHORT).show();
         }
@@ -39,5 +43,14 @@ public class StoreActivity extends AppCompatActivity {
         count = Save.load(getApplicationContext());
         points.setText(String.valueOf("Points: " + count));
     }
+// New skin
+    /*public void newSkin(View view) {
+        if (count >= 50) {
+            startActivity(new Intent(StoreActivity).this, NewSkinActivity.class));
+            count -= 50;
+        } else {
+            Toast.makeText(getApplicationContext(), "Not enough points", Toast.LENGTH_SHORT).show();
+        }
+    } */
 
 }
