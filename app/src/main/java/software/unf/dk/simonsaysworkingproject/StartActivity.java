a0b0d287a479a4872cb86a992c1d9858d2faa955
deltager <github@software.unf.dk>
2017-07-09 22:51:39 +0200

    Start screen and other layout changed.

--- app/src/main/java/software/unf/dk/simonsaysworkingproject/StartActivity.java
+++ app/src/main/java/software/unf/dk/simonsaysworkingproject/StartActivity.java
@@ -1,13 +1,23 @@
 package software.unf.dk.simonsaysworkingproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    private Button transition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        transition = (Button) findViewById(R.id.transition);
+    }
+
+    public void transition(View view) {
+        startActivity(new Intent(StartActivity.this, MainActivity.class));
     }
 }
